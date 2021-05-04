c5a449dee17d436d18884ad7a54ed4f6d8958afc
seonghyeoncho <chosh040@naver.com>
2021-05-04 17:29:23 +0900

    라우터 설정

---
+++ src/Router.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
+const RouterComponent: React.FC = () => (
+    <Switch>
+            
+    </Switch>
+)
+
+const AppRouter: React.FC = () => (
+    <Router>
+        <RouterComponent/>
+    </Router>
+)
+
+export default AppRouter;
